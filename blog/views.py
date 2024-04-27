48286cb59c19bb867e939adb20b440b18199a593
alireza <alirezaa073@gmail.com>
2024-04-27 20:05:13 +0430

    is authenticated or read only permisions added to person, category, product views added.

--- blog/views.py
+++ blog/views.py
@@ -3,33 +3,46 @@ from rest_framework.generics import ListCreateAPIView, \
 
 from blog.models import Person, Product, Category
 from blog.serializers import PersonSerializer, ProductSerializer, CategorySerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class PersonListCreateView(ListCreateAPIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
 
 class PersonRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
 
 class CategoryListCreateView(ListCreateAPIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class CategoryRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class ProductListCreateView(ListCreateAPIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
